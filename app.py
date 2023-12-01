7097df48aa8b2c9a41b9a310e28fb56e7042c1c0
Shinzou Kataoka <sk03092003marmzthu.u.m@yahoo.ne.jp>
2023-12-01 13:17:30 +0900

    message

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ def upload1_user_files():
 def create():
     if request.method == "POST":
         result = int(request.form.get("result"))
-        # baught_at = request.form.get("date")
+        baught_at = request.form.get("date")
         # POSTリクエストからresultを取得
         # タイムゾーンを指定して現在の日時を取得
         # current_user = current_user
@@ -183,7 +183,7 @@ def create():
             username=current_user.username,
             user_id=current_user.id,
             saved_at=current_time,
-            # baught_at=baught_at,
+            baught_at=baught_at,
         )
         # saved_at=current_time
         # データベースに登録
