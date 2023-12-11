61a0e27d76bcf42cc2576fb863a7d16c40e3a33c
Shinzou Kataoka <sk03092003marmzthu.u.m@yahoo.ne.jp>
2023-12-11 20:36:18 +0900

    message

--- model3.py
+++ model3.py
@@ -138,7 +138,7 @@ def detect_text(path):
             continue
     print(Sum)
     n_sum = process_string(Sum)
-    if max(n_sum)[0] == "4" and max(n_sum)[1:] in Sum:
+    if str(max(n_sum))[0] == "4" and int(str(max(n_sum))[1:]) in Sum:
         return [max(n_sum)[1:], date_process(date), detail]
     else:
         return [max(n_sum), date_process(date), detail]
